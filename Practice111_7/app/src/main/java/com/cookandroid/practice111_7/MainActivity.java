db331be7063d6c7ee2327e076f83e718d96684cc
kumass2020 <72965092+kumass2020@users.noreply.github.com>
2021-04-02 21:57:34 +0900

    RadioButton

--- Practice111_7/app/src/main/java/com/cookandroid/practice111_7/MainActivity.java
+++ Practice111_7/app/src/main/java/com/cookandroid/practice111_7/MainActivity.java
@@ -2,13 +2,80 @@ package com.cookandroid.practice111_7;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button btn1, btn2;
+    EditText et1;
+    RadioGroup rg1;
+    RadioButton rb1, rb2;
+    ImageView iv1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        setTitle("좀 그럴듯한 앱");
+
+        btn1 = (Button)findViewById(R.id.btn1);
+        btn2 = (Button)findViewById(R.id.btn2);
+        et1 = (EditText)findViewById(R.id.et1);
+        rg1 = (RadioGroup)findViewById(R.id.rg1);
+        rb1 = (RadioButton)findViewById(R.id.rb1);
+        rb2 = (RadioButton)findViewById(R.id.rb2);
+        iv1 = (ImageView)findViewById(R.id.iv1);
+
+        btn2.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+                Intent mIntent = new Intent(Intent.ACTION_VIEW, Uri.parse(String.valueOf(et1.getText())));
+            }
+        });
+
+////        rg1.setOnClickListener(new View.OnClickListener() {
+////
+////            @Override
+////            public void onClick(View view) {
+//                switch(rg1.getCheckedRadioButtonId()) {
+//                    case R.id.rb1:
+//                        iv1.setImageResource(R.drawable.q10);
+//                        break;
+//                    case R.id.rb2:
+//                        iv1.setImageResource(R.drawable.r11);
+//                        break;
+//                }
+////            }
+////        });
+
+        rb1.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+                iv1.setImageResource(R.drawable.q10);
+            }
+        });
+
+        rb2.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+                iv1.setImageResource(R.drawable.r11);
+            }
+        });
+
+
+
+
     }
 }
