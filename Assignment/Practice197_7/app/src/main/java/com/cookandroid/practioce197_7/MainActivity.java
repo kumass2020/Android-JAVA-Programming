a620e9dfe306a9e1ba129d226e7ed06aee4c042b
kumass2020 <72965092+kumass2020@users.noreply.github.com>
2021-04-03 23:56:22 +0900

    197p 7번 완성

--- Assignment/Practice197_7/app/src/main/java/com/cookandroid/practioce197_7/MainActivity.java
+++ Assignment/Practice197_7/app/src/main/java/com/cookandroid/practioce197_7/MainActivity.java
@@ -3,12 +3,57 @@ package com.cookandroid.practioce197_7;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button btn1;
+    CheckBox cb1, cb2, cb3;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button btn1 = (Button)findViewById(R.id.btn1);
+        CheckBox cb1 = (CheckBox)findViewById(R.id.cb1);
+        CheckBox cb2 = (CheckBox)findViewById(R.id.cb2);
+        CheckBox cb3 = (CheckBox)findViewById(R.id.cb3);
+
+        cb1.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
+                if (cb1.isChecked()) {
+                    btn1.setEnabled(true);
+                } else {
+                    btn1.setEnabled(false);
+                }
+            }
+        });
+
+        cb2.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
+                if (cb2.isChecked()) {
+                    btn1.setClickable(true);
+                } else {
+                    btn1.setClickable(false);
+                }
+            }
+        });
+
+        cb3.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
+                if (cb3.isChecked()) {
+                    btn1.setRotation(45);
+                } else {
+                    btn1.setRotation(0);
+                }
+            }
+        });
+
     }
 }
