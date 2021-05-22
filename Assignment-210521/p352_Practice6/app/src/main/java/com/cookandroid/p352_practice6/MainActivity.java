093f3e178e052377a3a9143a93e93c6b321831af
hoho <kumass2021@gmail.com>
2021-05-23 01:22:29 +0900

    sdas

--- Assignment-210521/p352_Practice6/app/src/main/java/com/cookandroid/p352_practice6/MainActivity.java
+++ Assignment-210521/p352_Practice6/app/src/main/java/com/cookandroid/p352_practice6/MainActivity.java
@@ -1,17 +1,22 @@
 package com.cookandroid.p352_practice6;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 
+import android.Manifest;
 import android.content.Context;
 import android.os.Bundle;
+import android.os.Environment;
 import android.view.View;
 import android.widget.Button;
 import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.util.Calendar;
 
@@ -21,8 +26,14 @@ public class MainActivity extends AppCompatActivity {
     Button btnWrite;
     String fileName;
 
+    // sd카드 경로를 지정
+    final String SDpath = Environment.getExternalStorageDirectory().getAbsolutePath();
+    final File myDir = new File(SDpath + "/myDiary");
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE}, MODE_PRIVATE);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         setTitle("간단 일기장");
@@ -36,7 +47,10 @@ public class MainActivity extends AppCompatActivity {
         int cMonth = cal.get(Calendar.MONTH);
         int cDay = cal.get(Calendar.DAY_OF_MONTH);
 
-        // 처음 실행 시 파일 이름 검사 
+        // 처음 실행 시 파일 이름 검사
+        if(!myDir.exists()) {
+            myDir.mkdir();
+        }
         fileName = Integer.toString(cYear) + "_" + Integer.toString(cMonth+1)
                 + "_" + Integer.toString(cDay) + ".txt";
         String str = readDiary(fileName);
@@ -55,12 +69,22 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 try {
-                    FileOutputStream outFs = openFileOutput(fileName, Context.MODE_PRIVATE);
+//                    myDir.mkdir();
+                    File file = new File(SDpath + "/" + fileName);
+                    FileOutputStream outFs = new FileOutputStream(file);
+//                    FileOutputStream outFs = openFileOutput(SDpath + fileName, Context.MODE_PRIVATE);
                     String str = edtDiary.getText().toString();
                     outFs.write(str.getBytes());
                     outFs.close();
+//                    if(!file.exists()) {
+//                        file.createNewFile();
+//                    }
+//                    FileWriter writer = new FileWriter(file, false);
+//                    writer.write(str);
+//                    writer.close();
                     Toast.makeText(getApplicationContext(), fileName+" 이 저장됨", Toast.LENGTH_SHORT).show();
                 } catch (IOException e) {
+                    System.out.println(e.getMessage());
                 }
             }
         });
@@ -70,8 +94,13 @@ public class MainActivity extends AppCompatActivity {
     String readDiary(String fName) {
         String diaryStr = null;
         FileInputStream inFs;
+        final String SDpath = Environment.getExternalStorageDirectory().getAbsolutePath();
+        final File myDir = new File(SDpath + "/myDiary");
+        File file = new File(SDpath, fileName);
+        myDir.mkdir();
         try {
-            inFs = openFileInput(fName);
+//            inFs = openFileInput("/sdcard/myDiary/" + fName);
+            inFs = new FileInputStream(SDpath + "/" + fName);
             byte[] txt = new byte[500];
             inFs.read(txt);
             inFs.close();
